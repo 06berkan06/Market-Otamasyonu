d717b944cc34a91862a7a2fdc940b3574f408d1e
berkan <berkan@06BERKAN-PC>
2024-04-24 00:23:41 +0300

    Proje dosyası ekle.

---
+++ Market Otamasyonu/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Market_Otamasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntumu = new System.Windows.Forms.Button();
+            this.btnmeyveler = new System.Windows.Forms.Button();
+            this.btnbakliyat = new System.Windows.Forms.Button();
+            this.btnsebzeler = new System.Windows.Forms.Button();
+            this.btndigerleri = new System.Windows.Forms.Button();
+            this.btnicecek = new System.Windows.Forms.Button();
+            this.btnkonserve = new System.Windows.Forms.Button();
+            this.btndondurulmus = new System.Windows.Forms.Button();
+            this.btnurunekle = new System.Windows.Forms.Button();
+            this.btnurunsılme = new System.Windows.Forms.Button();
+            this.btnurunguncel = new System.Windows.Forms.Button();
+            this.rtburunler = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbltutar = new System.Windows.Forms.Label();
+            this.lbladet = new System.Windows.Forms.Label();
+            this.txtadet = new System.Windows.Forms.TextBox();
+            this.rtbsepet = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsepetekle = new System.Windows.Forms.Button();
+            this.txtsepetekle = new System.Windows.Forms.TextBox();
+            this.txtsepetcikar = new System.Windows.Forms.TextBox();
+            this.btnürünsil = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btntumu
+            // 
+            this.btntumu.Location = new System.Drawing.Point(26, 12);
+            this.btntumu.Name = "btntumu";
+            this.btntumu.Size = new System.Drawing.Size(97, 61);
+            this.btntumu.TabIndex = 0;
+            this.btntumu.Text = "TÜMÜ";
+            this.btntumu.UseVisualStyleBackColor = true;
+            // 
+            // btnmeyveler
+            // 
+            this.btnmeyveler.Location = new System.Drawing.Point(129, 12);
+            this.btnmeyveler.Name = "btnmeyveler";
+            this.btnmeyveler.Size = new System.Drawing.Size(89, 61);
+            this.btnmeyveler.TabIndex = 1;
+            this.btnmeyveler.Text = "MEYVELER";
+            this.btnmeyveler.UseVisualStyleBackColor = true;
+            // 
+            // btnbakliyat
+            // 
+            this.btnbakliyat.Location = new System.Drawing.Point(319, 12);
+            this.btnbakliyat.Name = "btnbakliyat";
+            this.btnbakliyat.Size = new System.Drawing.Size(89, 61);
+            this.btnbakliyat.TabIndex = 3;
+            this.btnbakliyat.Text = "BAKLİYAT";
+            this.btnbakliyat.UseVisualStyleBackColor = true;
+            // 
+            // btnsebzeler
+            // 
+            this.btnsebzeler.Location = new System.Drawing.Point(224, 12);
+            this.btnsebzeler.Name = "btnsebzeler";
+            this.btnsebzeler.Size = new System.Drawing.Size(89, 61);
+            this.btnsebzeler.TabIndex = 2;
+            this.btnsebzeler.Text = "SEBZELER";
+            this.btnsebzeler.UseVisualStyleBackColor = true;
+            // 
+            // btndigerleri
+            // 
+            this.btndigerleri.Location = new System.Drawing.Point(699, 12);
+            this.btndigerleri.Name = "btndigerleri";
+            this.btndigerleri.Size = new System.Drawing.Size(99, 61);
+            this.btndigerleri.TabIndex = 7;
+            this.btndigerleri.Text = "DİĞERLERİ";
+            this.btndigerleri.UseVisualStyleBackColor = true;
+            // 
+            // btnicecek
+            // 
+            this.btnicecek.Location = new System.Drawing.Point(604, 12);
+            this.btnicecek.Name = "btnicecek";
+            this.btnicecek.Size = new System.Drawing.Size(89, 61);
+            this.btnicecek.TabIndex = 6;
+            this.btnicecek.Text = "İÇECEKLER";
+            this.btnicecek.UseVisualStyleBackColor = true;
+            // 
+            // btnkonserve
+            // 
+            this.btnkonserve.Location = new System.Drawing.Point(509, 12);
+            this.btnkonserve.Name = "btnkonserve";
+            this.btnkonserve.Size = new System.Drawing.Size(89, 61);
+            this.btnkonserve.TabIndex = 5;
+            this.btnkonserve.Text = "KONSERVELER";
+            this.btnkonserve.UseVisualStyleBackColor = true;
+            // 
+            // btndondurulmus
+            // 
+            this.btndondurulmus.Location = new System.Drawing.Point(414, 12);
+            this.btndondurulmus.Name = "btndondurulmus";
+            this.btndondurulmus.Size = new System.Drawing.Size(89, 61);
+            this.btndondurulmus.TabIndex = 4;
+            this.btndondurulmus.Text = "DONDURULMUS GIDA";
+            this.btndondurulmus.UseVisualStyleBackColor = true;
+            // 
+            // btnurunekle
+            // 
+            this.btnurunekle.Location = new System.Drawing.Point(26, 97);
+            this.btnurunekle.Name = "btnurunekle";
+            this.btnurunekle.Size = new System.Drawing.Size(111, 128);
+            this.btnurunekle.TabIndex = 8;
+            this.btnurunekle.Text = "ÜRÜN EKLE";
+            this.btnurunekle.UseVisualStyleBackColor = true;
+            // 
+            // btnurunsılme
+            // 
+            this.btnurunsılme.Location = new System.Drawing.Point(26, 231);
+            this.btnurunsılme.Name = "btnurunsılme";
+            this.btnurunsılme.Size = new System.Drawing.Size(111, 128);
+            this.btnurunsılme.TabIndex = 9;
+            this.btnurunsılme.Text = "ÜRÜN SİLME";
+            this.btnurunsılme.UseVisualStyleBackColor = true;
+            // 
+            // btnurunguncel
+            // 
+            this.btnurunguncel.Location = new System.Drawing.Point(26, 365);
+            this.btnurunguncel.Name = "btnurunguncel";
+            this.btnurunguncel.Size = new System.Drawing.Size(111, 128);
+            this.btnurunguncel.TabIndex = 10;
+            this.btnurunguncel.Text = "ÜRÜN GÜNCELLEME";
+            this.btnurunguncel.UseVisualStyleBackColor = true;
+            // 
+            // rtburunler
+            // 
+            this.rtburunler.Location = new System.Drawing.Point(143, 97);
+            this.rtburunler.Name = "rtburunler";
+            this.rtburunler.Size = new System.Drawing.Size(294, 339);
+            this.rtburunler.TabIndex = 11;
+            this.rtburunler.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ürünler";
+            // 
+            // lbltutar
+            // 
+            this.lbltutar.AutoSize = true;
+            this.lbltutar.Location = new System.Drawing.Point(449, 80);
+            this.lbltutar.Name = "lbltutar";
+            this.lbltutar.Size = new System.Drawing.Size(44, 13);
+            this.lbltutar.TabIndex = 13;
+            this.lbltutar.Text = "TUTAR";
+            // 
+            // lbladet
+            // 
+            this.lbladet.AutoSize = true;
+            this.lbladet.Location = new System.Drawing.Point(152, 445);
+            this.lbladet.Name = "lbladet";
+            this.lbladet.Size = new System.Drawing.Size(39, 13);
+            this.lbladet.TabIndex = 14;
+            this.lbladet.Text = "ADET:";
+            // 
+            // txtadet
+            // 
+            this.txtadet.Location = new System.Drawing.Point(197, 442);
+            this.txtadet.Name = "txtadet";
+            this.txtadet.Size = new System.Drawing.Size(100, 20);
+            this.txtadet.TabIndex = 15;
+            // 
+            // rtbsepet
+            // 
+            this.rtbsepet.Location = new System.Drawing.Point(466, 96);
+            this.rtbsepet.Name = "rtbsepet";
+            this.rtbsepet.Size = new System.Drawing.Size(332, 339);
+            this.rtbsepet.TabIndex = 16;
+            this.rtbsepet.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(446, 445);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "SEPETE EKLE";
+            // 
+            // btnsepetekle
+            // 
+            this.btnsepetekle.Location = new System.Drawing.Point(657, 438);
+            this.btnsepetekle.Name = "btnsepetekle";
+            this.btnsepetekle.Size = new System.Drawing.Size(56, 23);
+            this.btnsepetekle.TabIndex = 18;
+            this.btnsepetekle.Text = "+";
+            this.btnsepetekle.UseVisualStyleBackColor = true;
+            // 
+            // txtsepetekle
+            // 
+            this.txtsepetekle.Location = new System.Drawing.Point(551, 441);
+            this.txtsepetekle.Name = "txtsepetekle";
+            this.txtsepetekle.Size = new System.Drawing.Size(100, 20);
+            this.txtsepetekle.TabIndex = 19;
+            // 
+            // txtsepetcikar
+            // 
+            this.txtsepetcikar.Location = new System.Drawing.Point(551, 467);
+            this.txtsepetcikar.Name = "txtsepetcikar";
+            this.txtsepetcikar.Size = new System.Drawing.Size(100, 20);
+            this.txtsepetcikar.TabIndex = 22;
+            // 
+            // btnürünsil
+            // 
+            this.btnürünsil.Location = new System.Drawing.Point(657, 464);
+            this.btnürünsil.Name = "btnürünsil";
+            this.btnürünsil.Size = new System.Drawing.Size(56, 23);
+            this.btnürünsil.TabIndex = 21;
+            this.btnürünsil.Text = "-";
+            this.btnürünsil.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 470);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "SEPETTEN ÇIKAR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 518);
+            this.Controls.Add(this.txtsepetcikar);
+            this.Controls.Add(this.btnürünsil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtsepetekle);
+            this.Controls.Add(this.btnsepetekle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbsepet);
+            this.Controls.Add(this.txtadet);
+            this.Controls.Add(this.lbladet);
+            this.Controls.Add(this.lbltutar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtburunler);
+            this.Controls.Add(this.btnurunguncel);
+            this.Controls.Add(this.btnurunsılme);
+            this.Controls.Add(this.btnurunekle);
+            this.Controls.Add(this.btndigerleri);
+            this.Controls.Add(this.btnicecek);
+            this.Controls.Add(this.btnkonserve);
+            this.Controls.Add(this.btndondurulmus);
+            this.Controls.Add(this.btnbakliyat);
+            this.Controls.Add(this.btnsebzeler);
+            this.Controls.Add(this.btnmeyveler);
+            this.Controls.Add(this.btntumu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntumu;
+        private System.Windows.Forms.Button btnmeyveler;
+        private System.Windows.Forms.Button btnbakliyat;
+        private System.Windows.Forms.Button btnsebzeler;
+        private System.Windows.Forms.Button btndigerleri;
+        private System.Windows.Forms.Button btnicecek;
+        private System.Windows.Forms.Button btnkonserve;
+        private System.Windows.Forms.Button btndondurulmus;
+        private System.Windows.Forms.Button btnurunekle;
+        private System.Windows.Forms.Button btnurunsılme;
+        private System.Windows.Forms.Button btnurunguncel;
+        private System.Windows.Forms.RichTextBox rtburunler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbltutar;
+        private System.Windows.Forms.Label lbladet;
+        private System.Windows.Forms.TextBox txtadet;
+        private System.Windows.Forms.RichTextBox rtbsepet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsepetekle;
+        private System.Windows.Forms.TextBox txtsepetekle;
+        private System.Windows.Forms.TextBox txtsepetcikar;
+        private System.Windows.Forms.Button btnürünsil;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
